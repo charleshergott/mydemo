3356c1868e3d8b4e2299b27e71105edf3ad4d8d2
charleshergott <150687443+charleshergott@users.noreply.github.com>
2024-04-17 13:16:04 +0200

    commit_170424

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import database from './data.json';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'mydemo';
+  title = database.title;
+  categories = database.data;
+
+
+  handleClick($event: any) {
+    this.title = 'demo'
+  };
 }
