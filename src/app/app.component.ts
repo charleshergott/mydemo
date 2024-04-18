dfd5878a35797d48b70e0d5c8972807d6917265f
charleshergott <150687443+charleshergott@users.noreply.github.com>
2024-04-18 12:08:01 +0200

    commit_180424_1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,25 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import database from './data.json';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = database.title;
   categories = database.data;
+  recipes: any[] = [];
 
 
   handleClick($event: any) {
     this.title = 'demo'
   };
+  handleHeaderClick($event: any) {
+    console.log($event);
+  }
 }
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+  template: `<button (click)="emitEvent()">emit event</button>`,
+
+})
+export class HeaderComponent {
+  @Input() public title?: string;
+  @Output() public childEvent: EventEmitter<string> = new EventEmitter();
+  emitEvent() {
+    console.log('xxx');
+    this.childEvent.emit('event from child');
+  }
+}
+
+
+
