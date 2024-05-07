f3e51ad425ab9b0e5e56a7a9a1a3fab8b183ce94
charleshergott <150687443+charleshergott@users.noreply.github.com>
2024-05-07 12:36:23 +0200

    commit_070524_3

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule, IonicModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss',
+
+})
+
+export class FooterComponent {
+
+}
+
+
+
+
